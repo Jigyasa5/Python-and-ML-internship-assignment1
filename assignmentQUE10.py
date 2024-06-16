fb913cf8d9a6fb8671c825fcbc96e5fb7235c0b2
Jigyasa5 <144715415+Jigyasa5@users.noreply.github.com>
2024-06-16 20:20:30 +0530

    Add files via upload

---
+++ assignmentQUE10.py
@@ -0,0 +1,4 @@
+# QUE 10) Write a python program that converts a given string to uppercase.
+string = input("Enter string to convert into uppercase: ")
+upper_string = string.upper()
+print("String in uppercase: ",upper_string)
