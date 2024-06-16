ba8d2aa9cde6176bb54e7c753fb24be9dfc4c8e8
Jigyasa5 <144715415+Jigyasa5@users.noreply.github.com>
2024-06-16 20:19:03 +0530

    Add files via upload

---
+++ assignmentQUE7.py
@@ -0,0 +1,4 @@
+# QUE 7) Write a python program that takes a string as input and returns its length.
+string = input("Enter a string: ")
+length_of_string = len(string)
+print("Length of string is: ", length_of_string)
