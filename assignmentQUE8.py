558a27216d5d32d9a69e8087f7d93b2972a13a5e
Jigyasa5 <144715415+Jigyasa5@users.noreply.github.com>
2024-06-16 20:19:28 +0530

    Add files via upload

---
+++ assignmentQUE8.py
@@ -0,0 +1,5 @@
+# QUE 8) Write a python program that concatenates two strings and returns the result.
+string1 = input("enter the string1: ")
+string2 = input("enter the string2: ")
+result = string1 + string2
+print("Concatenated string: ",result)
